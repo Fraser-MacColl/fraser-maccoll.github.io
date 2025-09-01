f0d4c4cec6f8fdd68254addb348d91236982efdc
Fraser-MacColl <fraser.a.maccoll@gmail.com>
2025-09-01 23:12:52 +0100

    Updated to use tailwind css

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,14 @@
 use leptos::prelude::*;
+use leptos_meta::Stylesheet;
 
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
+        <Stylesheet id="leptos" href="output.css"/>
         <button
+            class="p-2 text-red"
             on:click=move |_| set_count.update(|val| *val+=1)
         >
             "Click me: "
