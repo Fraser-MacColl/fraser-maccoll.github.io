646bf49d387058797197e074bd614e8f3ab79765
Fraser-MacColl <fraser.a.maccoll@gmail.com>
2025-09-03 22:52:36 +0100

    Update styling

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,10 @@
 use leptos::prelude::*;
-use leptos_meta::Stylesheet;
 
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <Stylesheet id="leptos" href="output.css"/>
         <button
             class="p-32 text-red-600"
             on:click=move |_| set_count.update(|val| *val+=1)
